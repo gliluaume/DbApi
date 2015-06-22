aa901afd2a4ffafa6fccbf5433a8a8093ca47075
gliluaume <salicisguillaume@yahoo.fr>
2015-06-22 19:42:32 +0200

    initial

---
+++ Mapper/OraCommandDescriptor.cs
@@ -0,0 +1,23 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiBdd.Mapper
+{
+    public class OraCommandDescriptor : ICommandDescriptor
+    {
+        public IEnumerable<IDataParameter> CommandParameters { get; set; }
+        public string CommandText { get; set; }
+        public CommandType CommType { get; set; }
+        public OraCommandDescriptor()
+        {
+            this.CommandParameters = new List<OracleParameter>();
+            this.CommandText = string.Empty;
+            this.CommType = CommandType.StoredProcedure;
+        }
+    }
+}
