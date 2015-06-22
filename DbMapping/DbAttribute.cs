43824d89c60c8c5f7f77209127ab8852fc305ba1
gliluaume <salicisguillaume@yahoo.fr>
2015-06-22 19:44:10 +0200

    initial

---
+++ DbMapping/DbAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiBdd.DbMapping
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class DbAttribute : Attribute
+    {
+        private string name;
+        private bool isMandatory;
+        public double version = 1.0;
+
+        public DbAttribute(string name)
+        {
+            this.name = name;
+            this.isMandatory = false;
+        }
+        public DbAttribute(string name, bool mandatory)
+        {
+            this.name = name;
+            this.isMandatory = mandatory;
+        }
+        public string Name { get { return name; } }
+        public bool IsMandatory { get { return isMandatory; } }
+    }
+}
---
+++ DbMapping/DbObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiBdd.DbMapping
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public class DbObject : Attribute
+    {
+        string name;
+        public double version;
+
+        public DbObject(string name)
+        {
+            this.name = name;
+
+            // Default value.
+            version = 1.0;
+        }
+
+        public string Name { get { return name; } }
+    }
+}
