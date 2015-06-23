f51c861d30f88a5ea9252f3d8180dac3d7b944ff
gliluaume <salicisguillaume@yahoo.fr>
2015-06-23 09:11:59 +0200

    Update DbAttribute.cs

--- DbMapping/DbAttribute.cs
+++ DbMapping/DbAttribute.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApiBdd.DbMapping
+namespace DbApi.DbMapping
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class DbAttribute : Attribute
