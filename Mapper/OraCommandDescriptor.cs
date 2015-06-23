bf1ffcb40ddae246aa49226f43d1a735a03186bc
gliluaume <salicisguillaume@yahoo.fr>
2015-06-23 09:12:48 +0200

    Update OraCommandDescriptor.cs

--- Mapper/OraCommandDescriptor.cs
+++ Mapper/OraCommandDescriptor.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ApiBdd.Mapper
+namespace DbApi.Mapper
 {
     public class OraCommandDescriptor : ICommandDescriptor
     {
